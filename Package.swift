96df5cbf4f2dfd4caa243905aa8715028a89ed95
Christian <83311493+xatitive@users.noreply.github.com>
2024-09-01 01:29:42 -0400

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,17 @@
+// swift-tools-version: 5.9
+import PackageDescription
+
+let package = Package(
+    name: "Tracker",
+    products: [
+        .executable(
+            name:"Tracker",
+            targets: ["Tracker"]
+        )
+    ],
+    targets: [
+        .executableTarget(
+            name: "Tracker"
+        )
+    ]
+)
---
+++ Tracker/InstaAPI.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  Tracker
+//
+//  Created by reagan m on 9/1/24.
+//
+
+import Foundation
+import Swiftagram
+
+class InstaAPI {
+ 	
+}
